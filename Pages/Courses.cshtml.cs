addc511b5c7a0d01a072d26af18915e3eb9b795b
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-06-12 21:04:29 -0500

    Mostrar elementos en pantalla con errores

---
+++ Pages/Courses.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListaCursos.Interfaces;
+using ListaCursos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ListaCursos.Pages
+{
+    public class CoursesModel : PageModel
+    {
+        public readonly ICoursesProvider coursesProvider;
+        public List<Course> Courses { get; set; }
+
+        public CoursesModel(ICoursesProvider coursesProvider)
+        {
+            this.coursesProvider = coursesProvider;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var results = await coursesProvider.GetAllAsync();
+            if(results != null)
+            {
+                Courses = new List<Course>(results);
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Providers/FakeCoursesProvider.cs
@@ -0,0 +1,35 @@
+﻿using ListaCursos.Interfaces;
+using ListaCursos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaCursos.Providers
+{
+    public class FakeCoursesProvider : ICoursesProvider
+    {
+        private readonly List<Course> repo = new List<Course>();
+        public FakeCoursesProvider()
+        {
+            repo.Add(new Course()
+            {
+                Id = 1,
+                Name = "Curso de ASP.NET Escencial",
+                Author = "José Daniel Posso García",
+                Description = "El mejor curso del mundo",
+                Uri = "http://linkedin.com/learning"
+            });
+        }
+
+        public Task<ICollection<Course>> GetAllAsync()
+        {
+            return Task.FromResult((ICollection<Course>)repo.ToList());
+        }
+
+        public Task<Course> GetAsync(int id)
+        {
+            return Task.FromResult(repo.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
